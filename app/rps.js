afad127dc0936880dd654a4522b230dbefa8d9b1
RaviHelon-msf <ravihelon@gmail.com>
2023-10-20 17:35:23 -0300

    Feat: Sadness

--- app/rps.js
+++ app/rps.js
@@ -2,19 +2,19 @@ let rps = () => {
     let rps_options = ['rock', 'paper', 'scissors']
     let pc_choice = ~~(Math.random()*rps_options.length)
 
-    alert("Computer boy already chose. Choose between Rock Paper and Scissors:")
+    console.log("Computer boy already chose. Choose between Rock Paper and Scissors:")
     let player_choice = rps_options.indexOf(prompt().toLowerCase())
 
     if(player_choice == -1) {
-        alert("That is not a valid RPS choice. Please try again.")
+        console.log("That is not a valid RPS choice. Please try again.")
         return rps()
     }
     else if(player_choice == pc_choice){
-        alert(`The Computer also chose ${rps_options[pc_choice]}.\n That's a Draw, please choose again.`)
+        console.log(`The Computer also chose ${rps_options[pc_choice]}.\n That's a Draw, please choose again.`)
         return rps()
     }
 
-    alert(`Wow. The Computer boy chose ${rps_options[pc_choice]}`)
+    console.log(`Wow. The Computer boy chose ${rps_options[pc_choice]}`)
     return ((pc_choice-player_choice)%3)*2-3
 }
 
@@ -22,7 +22,7 @@ let game = () => {
     let score = 0
     
     while(1) {
-        alert(`The new score is now ${score}`)
+        console.log(`The new score is now ${score}`)
         if( score >= 3)
             return "Hey. Thats a wrap. You Win!"
 
@@ -38,13 +38,31 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-alert("Hello Operator. Do you want to play a game?")
+console.log("Hello Operator. Do you want to play a game?")
 
-alert("LOL. Don't Worry, it's just a RPS. I'll be the moderator.")
+console.log("LOL. Don't Worry, it's just a RPS. I'll be the moderator.")
 
-positive = ['yes', 'y', 'ok', 'sim', 'blz']
+// game()
 
-if(positive.includes(prompt().toLowerCase)){
-    game()
-}
-
+function getUserInput() {
+    return new Promise((resolve) => {
+      const message = 'Please enter something:';
+      console.log(message);
+  
+      const intervalId = setInterval(() => {
+        const userInput = prompt(message);
+  
+        if (userInput !== null) {
+          clearInterval(intervalId);
+          console.log(`You entered: ${userInput}`);
+          resolve(userInput);
+        }
+      }, 100);
+    });
+  }
+  
+  (async () => {
+    const userInput = await getUserInput();
+    console.log(`You can now use the input: ${userInput}`);
+  })();
+  
