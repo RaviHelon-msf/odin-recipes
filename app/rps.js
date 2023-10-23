9f3eb0817af7cd1d566d750e8a36dd80e6d90136
RaviHelon-msf <ravihelon@gmail.com>
2023-10-23 09:22:19 -0300

    feat: RPS game complete
    RPS does not pop-up in the page, unless prompted by player.

--- app/rps.js
+++ app/rps.js
@@ -2,27 +2,31 @@ let rps = () => {
     let rps_options = ['rock', 'paper', 'scissors']
     let pc_choice = ~~(Math.random()*rps_options.length)
 
-    console.log("Computer boy already chose. Choose between Rock Paper and Scissors:")
-    let player_choice = rps_options.indexOf(prompt().toLowerCase())
+    alert("Computer boy already chose.")
+    let player_choice = rps_options.indexOf(prompt("Choose between Rock Paper and Scissors:").toLowerCase())
 
     if(player_choice == -1) {
-        console.log("That is not a valid RPS choice. Please try again.")
+        alert("That is not a valid RPS choice. Please try again.")
         return rps()
     }
     else if(player_choice == pc_choice){
-        console.log(`The Computer also chose ${rps_options[pc_choice]}.\n That's a Draw, please choose again.`)
+        alert(`The Computer also chose ${rps_options[pc_choice]}.\n That's a Draw, please choose again.`)
         return rps()
     }
 
-    console.log(`Wow. The Computer boy chose ${rps_options[pc_choice]}`)
+    alert(`Wow. The Computer boy chose ${rps_options[pc_choice]}`)
     return ((pc_choice-player_choice)%3)*2-3
+    //  Estamos trabalhando com um corpo de 3 elementos. Podemos os comparar 
+    // em base 3
+    //  A conversão multiplicando por 2 e tirando 3 é para melhor distinguir 
+    // as alternativas em relação à escolhida
 }
 
 let game = () => {
     let score = 0
     
     while(1) {
-        console.log(`The new score is now ${score}`)
+        alert(`The new score is now ${score}`)
         if( score >= 3)
             return "Hey. Thats a wrap. You Win!"
 
@@ -42,27 +46,4 @@ console.log("Hello Operator. Do you want to play a game?")
 
 console.log("LOL. Don't Worry, it's just a RPS. I'll be the moderator.")
 
-// game()
-
-function getUserInput() {
-    return new Promise((resolve) => {
-      const message = 'Please enter something:';
-      console.log(message);
-  
-      const intervalId = setInterval(() => {
-        const userInput = prompt(message);
-  
-        if (userInput !== null) {
-          clearInterval(intervalId);
-          console.log(`You entered: ${userInput}`);
-          resolve(userInput);
-        }
-      }, 100);
-    });
-  }
-  
-  (async () => {
-    const userInput = await getUserInput();
-    console.log(`You can now use the input: ${userInput}`);
-  })();
-  
+console.log("Write game() to play")
