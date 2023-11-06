51c5e3f648ee99884ca77354d62c2897b136be76
RaviHelon-msf <ravihelon@gmail.com>
2023-11-06 11:40:11 -0300

    Feat: Add button template
    Added a button and positioned it adequately. Working as a charm

--- app/hp12c/calculator.js
+++ app/hp12c/calculator.js
@@ -1,10 +1,23 @@
 let number = 10
 let history = Array(number).fill(0);
 
-const ul_history = document.getElementById('history')
+function display(history) {
 
-for (let i=0; i< number; i++){
-    const li = document.createElement('li')
-    li.textContent = '\#' + i + ': ' + history[i]
-    ul_history.appendChild(li)
+    const ul_history = document.getElementById('history')
+    ul_history.innerHTML = ''
+
+
+    for (let i = history.length-1; i >= 0; i--){
+        const li = document.createElement('li')
+        li.textContent = '\#' + (i+1) + ': ' + history[i]
+        ul_history.appendChild(li)
+    }
 }
+
+function history_update (num, hist=history) {
+    hist.shift()
+    hist.push(num)
+    display(hist)
+}
+
+display(history)
